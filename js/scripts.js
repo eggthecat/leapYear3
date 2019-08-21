6c0b719042ba0146fc8f0eb445ee7f19626787ab
dominic montelongo <dominic@>
2019-08-21 14:02:33 -0700

    Add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 // business logic
 var leapYear = function(year) {
-  if (year % 4 === 0) {
-    return true
+  if (year % 100 === 0){
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
   } else {
     return false;
-  };
+  }
 };
 
+
 // user interface logic
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#formOne").submit(function(event){
     event.preventDefault();
     var inputYear = parseInt($("input#year").val());
     var result  = leapYear(year);
-    console.log(result);
       $("#result").text(result);
   });
 });
