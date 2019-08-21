76c6616c010dc12cd512429394473c63702ba0a5
dominic montelongo <dominic@>
2019-08-21 13:30:52 -0700

    Modify busisness logic/user interface logic and adjust first if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
-// bl
+// business logic
 var leapYear = function(year) {
-  if (year %4===0 && year%400===0) {
+  if (year % 4 === 0) {
     return true
   } else {
     return false;
   };
 };
 
-// uil
+// user interface logic
 $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
     var inputYear = parseInt($("input#year").val());
-    console.log(inputYear);
     var result  = leapYear(year);
     console.log(result);
       $("#result").text(result);
