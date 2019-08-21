b374bffe6be7930e79cdd307d46328cc61f462fa
dominic montelongo <dominic@>
2019-08-21 08:45:12 -0700

    Add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+(document).ready(function(){
+  $("form#leapYear").submit(function(event){
+    event.preventDefault();
+
+    var inputYear = parseInt$("#year").val();
+
+    var outputYear =
+  })
+  });
